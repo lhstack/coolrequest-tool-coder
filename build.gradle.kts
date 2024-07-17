ae0aa7637a2147d81b8d4de6f8ed43f8e5f3e3f6
lhstack <lhstack@foxmail.com>
2024-07-17 18:08:46 +0800

    v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.coolrequest.tool.coder"
-version = "1.0-SNAPSHOT"
+version = "v1.0.0"
 
 repositories {
     mavenLocal()
@@ -20,7 +20,7 @@ intellij {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation(fileTree("/libs"))
+    implementation(fileTree("$projectDir/libs"))
 }
 
 tasks {
